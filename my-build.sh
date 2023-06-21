e669c164caafe53644bb94fbeed0faca08aaad2e
danhantao <danhantao@yeah.net>
2023-06-21 17:13:54 +0800

    update my-build.sh

--- my-build.sh
+++ my-build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-export NDK=$HOME/Library/Android/sdk/ndk/23.0.7599858
+export NDK=$NDK_HOME
 export HOST_TAG=darwin-x86_64
 export MIN_SDK_VERSION=23
+export ANDROID_NDK_ROOT=$NDK_HOME
 
 export CFLAGS="-Os"
 export LDFLAGS="-Wl,-Bsymbolic"
